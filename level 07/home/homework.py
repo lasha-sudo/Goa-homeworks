fda73ebd7bae2072109a278862a1ec2db89bbbd5
lasha-sudo <mekokishvili1011@gmail.com>
2024-11-11 19:36:37 +0400

    second commit

---
+++ level 07/home/homework.py
@@ -0,0 +1,44 @@
+name = input("enter your name: " )
+last_name = input("enter your last name: ")
+age = input("enter your name: ")
+country = input("enter your country: ")
+city = input("enter your city: ")
+fav_color = input("enter your fav_color:")
+fav_car =input("enter your fav_color: ")
+print("My name is " + name + " " + "my last name is " + last_name + " " + "i am " + str(age) + "years old " + " " + "i am from " + country + "i live in " + city + " " + "my fav color is " + fav_color + " " + "my favorite car is " + fav_car)
+
+
+
+
+#ყველა მათემათიკურ ოპერატორზე გააკეთეთ 10 მაგალითი, რიცხვები შემოიტანეთ input - ით. - (+,-,*,//)
+
+x = input("enter your first number:")
+y = input("enter your second number:")
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+o = input("enter your first number:")
+l = input("enter your second number:")
+print(o + l)
+print(0 - l)
+print(0 * l)
+print(0 / l)
+
+k = input("enter your first number:")
+b = input("enter your second number:")
+print(k + b)
+print(k - b)
+print(k * b)
+print(k / b)
+
+
+
+
+#ახსენით კომენტარების დახმარებით int(), str(), type() და input() ფუნქცია.
+
+#int()-ეს ფუნქცია აკეთებს იმას რომ სტრინგი ან ფლოათი გადააქციოს ინტეჯერად ანუ სტრინგად
+#str()-ეს ფუნქცია  აკეტებს იმას რომ ინტეჯერს ან ფლოათს გადააქცევს სტრინგად
+#type-ფუნქციით განსაზღვრავთ თუ როგორია მოცემული რამ სიტყვა, მთელი რიცხვია, სტრინგია ან ფლოათია
+#input-ფუნქციით შეგვიძლია ის რომ მომხამრებელს შემოვატანინოთ რაიმე მნიშვნელობა მაგალითად სახელი, გვარი ასაკი...
