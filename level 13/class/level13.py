e86badf4bc0335cb046801c54f850c498add508d
lasha-sudo <mekokishvili1011@gmail.com>
2024-11-25 22:16:01 +0400

    second commit

---
+++ level 13/class/level13.py
@@ -0,0 +1,66 @@
+#selection-არის არჩევანი
+#sequence-მიყვება ყველაფერს თანმიმდევრობით
+#interation-გამეორება loop შეგვიძლია ვუწოდოთ
+
+number = int(input("choose a number between 1 and 10: "))
+
+if number > 5:
+    print("you are begginer")
+elif number < 5:
+    print("you are pro")
+
+
+#while loop დახმარებით, 50 - ჯერ გამოიტანეთ 'GOA'
+age = 50
+
+while age > 0:
+    print("GOa")
+    age = age - 1
+
+
+
+
+
+
+
+
+
+
+
+
+#while ციკლის დახმარებით გამოიტანეღ 1 - დან 30 - მდე მხოლოდ ლუწი რიცხვები
+
+
+count = 30
+
+while count > 0:
+    if count % 2 == 0:
+        print(count)
+    count -= 1
+
+
+#while ციკლის დახმარებით, გამოიტანეთ რიცხვები  50 - დან 0 - მდე
+
+
+house = 50
+
+while house > 0:
+    if count % 1 ==0:
+        print(house)
+    house -= 1
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
